6c4c6826224bbb46898d7f09432d99bc37c01a6a
SkinlessMilk3 <45498940+SkinlessMilk3@users.noreply.github.com>
2020-04-12 20:17:45 -0500

    Add files via upload

--- Cyberpunk 2077 Countdown Timer/src/sample/Controller.java
+++ Cyberpunk 2077 Countdown Timer/src/sample/Controller.java
@@ -1,15 +1,85 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
-public class Controller {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
     @FXML
-    private Button btn;
+    private Button playBtn;
+
+    @FXML
+    private Button stopBtn;
+
+    @FXML
+    private Button choosePlaylist;
+
+    @FXML
+    private GridPane root;
+
+    @FXML
+    private Label timer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Main obj = new Main();
+
+        Image img = new Image(getClass().getResourceAsStream("metro-en.jpg"));
+        root.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+
+        timer.setTextFill(Color.WHITE);
+        timer.setFont(new Font("Timers New Roman", 30));
+
+        obj.Countdown(timer);
+
+        //Choose playlist for music
+        choosePlaylist.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                obj.setPlayer();
+            }
+        });
+
+        //Play music
+        playBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(playBtn.getText().compareTo("Play") == 0) {
+                    if(obj.play() == 0)
+                        playBtn.setText("Pause");
+                }
+                else{
+                    playBtn.setText("Play");
+                }
+            }
+        });
+
+        //Stop music. Probably should get rid of this because it's pretty useless.
+        stopBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) { obj.MusicStop(); }
+        });
 
 
-    public void initialize() {
-        btn.setText("asdfasf");
     }
-
+    public Button getChoosePlaylist(){
+        return choosePlaylist;
+    }
 }
--- Cyberpunk 2077 Countdown Timer/src/sample/Main.java
+++ Cyberpunk 2077 Countdown Timer/src/sample/Main.java
@@ -6,7 +6,9 @@ import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -58,51 +60,95 @@ public class Main extends Application {
     }
 
     //List of songs to be played.
-    List<MediaPlayer> players = new ArrayList<>();
+    private List<MediaPlayer> players = new ArrayList<>();
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
+        stage = primaryStage;
 
-        GridPane root = new GridPane();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));//new GridPane();
         primaryStage.setMaximized(true);
         primaryStage.setTitle("Cyberpunk 2077 Countdown");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
-        Button chooseMusic = new Button("Pick playlist");
-        DirectoryChooser directoryExplorer = new DirectoryChooser();
-        directoryExplorer.setTitle("Playlist");
-
-        chooseMusic.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-
-                setPlayer(directoryExplorer.showDialog(primaryStage));
-            }
-        });
-
-        Label timer = new Label();
-        timer.setFont(new Font("TimesNewRoman", 30));
-        Image img = new Image(getClass().getResourceAsStream("metro-en.jpg"));
-
-        timer.setTextFill(Color.WHITE);
-        root.setBackground(new Background(new BackgroundImage(img, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-
-
-
-        root.setAlignment(Pos.CENTER);
-
-        root.getChildren().add(timer);
-        //root.add(chooseMusic, 0,0);
-        //root.getChildren().add(chooseMusic);
-        
-        Countdown(timer);
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    protected void setPlayer(){
+
+        DirectoryChooser ch = new DirectoryChooser();
+        ch.setTitle("Choose playlist");
+        File dir = ch.showDialog(stage);
+
+        for(String file : dir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                for(String ch : SUPPORTED_FILE_EXTENSIONS)
+                    if(s.endsWith(ch)){
+
+
+                        return true;
+                    }
+
+                return false;
+            }
+        })){
+            String path = "file:///" + (dir + "\\" + file).replace("\\", "/").replaceAll(" ", "%20");
+            players.add(new MediaPlayer(new Media(path)));
+        }
+    }
+
+    //problem: Resets to the beginning of playlist.
+    protected void MusicStop(){
+
+        MediaPlayer.Status status = MediaPlayer.Status.PLAYING;
+        for(MediaPlayer player : players){
+            if(player.getStatus().equals(status) ){
+                player.stop();
+            }
+        }
+    }
+
+    protected int play() {
+        if (players.size() > 0) {
+            Task tsk = new Task() {
+                @Override
+                protected Void call() throws Exception {
+                    for (int i = 0; i < players.size(); i++) {
+                        if (i % players.size() == 0) {
+                            i = 0;
+                        }
+
+                        final MediaPlayer player = players.get(i);
+                        final MediaPlayer nxtPlayer = players.get((i + 1) % players.size());
+                        players.get(i).setOnEndOfMedia(new Runnable() {
+                            @Override
+                            public void run() {
+                                player.stop();
+                                nxtPlayer.play();
+                            }
+                        });
+
+                    }
+                    players.get(0).play();
+                    return null;
+                }
+            };
+
+            Thread th = new Thread(tsk);
+            th.setDaemon(true);
+            th.start();
+
+            return 0;
+        }
+        return 1;
     }
 
     protected void Countdown(Label timer){
@@ -173,56 +219,5 @@ public class Main extends Application {
         int remTime = 60 -  time.getSecond();
 
         return remTime;
-    }
-
-    protected void setPlayer(File dir){
-
-        for(String file : dir.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File file, String s) {
-                for(String ch : SUPPORTED_FILE_EXTENSIONS)
-                if(s.endsWith(ch)){
-
-
-                    return true;
-                }
-
-                return false;
-            }
-        })){
-            String path = "file:///" + (dir + "\\" + file).replace("\\", "/").replaceAll(" ", "%20");
-            players.add(new MediaPlayer(new Media(path)));
-        }
-        play();
-    }
-
-    protected void play(){
-        Task tsk = new Task() {
-            @Override
-            protected Void call() throws Exception {
-                for(int i = 0; i < players.size(); i++){
-                    if(i%players.size() == 0){
-                        i = 0;
-                    }
-
-                    final MediaPlayer player = players.get(i);
-                    final MediaPlayer nxtPlayer = players.get((i+1)%players.size());
-                    players.get(i).setOnEndOfMedia(new Runnable() {
-                        @Override
-                        public void run() {
-                            player.stop();
-                            nxtPlayer.play();
-                        }
-                    });
-
-                }
-                players.get(0).play();
-                return null;
-            }
-        };
-
-        Thread th = new Thread(tsk);
-        th.setDaemon(true);
-        th.start();
     }
 }
